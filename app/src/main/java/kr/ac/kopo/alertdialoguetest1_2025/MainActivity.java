d580997c23dc741691fd09e2d1a06f2b60cfad47
leehanseock <leehanseock@gmail.com>
2025-06-16 14:22:17 +0900

    기본대화상자

---
+++ app/src/main/java/kr/ac/kopo/alertdialoguetest1_2025/MainActivity.java
@@ -0,0 +1,48 @@
+package kr.ac.kopo.alertdialoguetest1_2025;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("인공지능소프트웨어과 공지사항");
+                dlg.setMessage("513호에 타과 학생이 만약에 있다면 추방해주시기 바랍니다.");
+                dlg.setIcon(R.drawable.blackcat);
+                dlg.setPositiveButton("확인", null);
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "취소버튼 클릭", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        dlg.show();
+            }
+        });
+    }
+}
