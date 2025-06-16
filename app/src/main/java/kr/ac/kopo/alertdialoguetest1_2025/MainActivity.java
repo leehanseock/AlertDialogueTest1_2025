b0140323ce46a41c21de8e327083e54e8e67795f
leehanseock <leehanseock@gmail.com>
2025-06-16 14:54:40 +0900

    대화상자 항목(item)표시

--- app/src/main/java/kr/ac/kopo/alertdialoguetest1_2025/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/alertdialoguetest1_2025/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -27,20 +28,42 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button btn1 = findViewById(R.id.btn1);
+        ImageView imgv = findViewById(R.id.imgv);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String[] hollweenIcons={"해골","좀비","박쥐","검은 고양이"};
                 AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
                 dlg.setTitle("인공지능소프트웨어과 공지사항");
-                dlg.setMessage("513호에 타과 학생이 만약에 있다면 추방해주시기 바랍니다.");
-                dlg.setIcon(R.drawable.blackcat);
-                dlg.setPositiveButton("확인", null);
-                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(MainActivity.this, "취소버튼 클릭", Toast.LENGTH_SHORT).show();
-                            }
-                        });
+                dlg.setItems(hollweenIcons, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        btn1.setText(hollweenIcons[which]);
+                        switch (which){
+                            case 0:
+                                imgv.setImageResource(R.drawable.skull);
+                                break;
+                            case 1:
+                                imgv.setImageResource(R.drawable.zombie);
+                                break;
+                            case 2:
+                                imgv.setImageResource(R.drawable.bat);
+                                break;
+                            case 3:
+                                imgv.setImageResource(R.drawable.blackcat);
+                                break;
+                        }
+                    }
+                });
+//                dlg.setMessage("513호에 타과 학생이 만약에 있다면 추방해주시기 바랍니다.");
+                dlg.setIcon(R.drawable.night);
+                dlg.setPositiveButton("닫기", null);
+//                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                Toast.makeText(MainActivity.this, "취소버튼 클릭", Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
                         dlg.show();
             }
         });
